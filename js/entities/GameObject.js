24a54962d3d8edf4570202e26a242186b5510b48
usercuong77 <111169630+usercuong77@users.noreply.github.com>
2026-01-24 00:05:12 +0700

    Delete js directory

--- js/entities/GameObject.js
+++
@@ -1,8 +0,0 @@
-// Auto-extracted from original HTML
-
-export class GameObject {
-    constructor(x, y, radius) {
-        this.x = x || 0; this.y = y || 0; this.radius = radius; this.markedForDeletion = false;
-    }
-    validatePosition() { if (isNaN(this.x)) this.x = 0; if (isNaN(this.y)) this.y = 0; }
-}
